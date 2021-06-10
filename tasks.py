008f50baf6bb671c1fc35e59fffe8e8a6e8fc1aa
abacigalup <abacigalup@eventbrite.com>
2021-06-10 15:22:51 -0300

    added circleci config

--- tasks.py
+++ tasks.py
@@ -2,7 +2,7 @@ from invoke_release.tasks import *  # noqa: F403
 
 
 configure_release_parameters(  # noqa: F405
-    module_name='facebook-python-sdk',
+    module_name='facebook',
     display_name='facebook-python-sdk',
     use_pull_request=True,
     use_tag=False,
